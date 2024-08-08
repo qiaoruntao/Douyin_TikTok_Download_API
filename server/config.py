18648ace4242861f16690354f5c7e0dc645a7edc
qiaoruntao <qiaoruntao01@gmail.com>
2024-08-09 03:06:39 +0800

    Load config from mongodb

--- server/config.py
+++ server/config.py
@@ -1,10 +1,49 @@
 # 配置文件路径
+import asyncio
 import os
+import platform
 
-import yaml
+from motor.motor_asyncio import AsyncIOMotorClient
 
-path = os.path.abspath(os.path.dirname(__file__))
-config_file_path = f"{path}/config.yaml"
-# 读取配置文件
-with open(config_file_path, "r", encoding="utf-8") as f:
-    config = yaml.safe_load(f)
+import crawlers.douyin.web.utils
+
+custom_config = None
+
+hostname = platform.node() or 'default'
+print(hostname)
+
+
+async def manage_config():
+    client = AsyncIOMotorClient(os.environ['MongoDbConfigStr'])
+    print("client connected")
+    change_stream = client.MConfig.DouYinWeb.watch([{
+        '$match': {
+            'operationType': {'$in': ['update']},
+            'fullDocument.key': {'$eq': hostname},
+        }
+    }], full_document="updateLookup")
+    print("watching")
+    global custom_config
+    document = await client.MConfig.DouYinWeb.find_one({'key': {'$eq': hostname}})
+    if document is None:
+        print("config document not found")
+        exit(1)
+    custom_config = document['data']
+    override_douyin_download_api_config(custom_config)
+    print("initial config fetched")
+    async for change in change_stream:
+        print(change)
+        custom_config = change['fullDocument']['data']
+        override_douyin_download_api_config(custom_config)
+
+
+def override_douyin_download_api_config(config):
+    crawlers.douyin.web.utils.config = config
+    crawlers.douyin.web.web_crawler.config = config
+
+
+if __name__ == '__main__':
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(manage_config())
+    loop.run_forever()
