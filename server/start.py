af39b1f9764716a954d086d5aca2455750d93771
qiaoruntao <qiaoruntao01@gmail.com>
2024-08-09 03:06:39 +0800

    Load config from mongodb

--- server/start.py
+++ server/start.py
@@ -1,8 +1,12 @@
+import asyncio
+
 import httpx
+from uvicorn import Config, Server
+
+from server.config import manage_config
 
 httpx.Client.__init__.__kwdefaults__['verify'] = False
 httpx.AsyncClient.__init__.__kwdefaults__['verify'] = False
-import uvicorn
 from fastapi import APIRouter, FastAPI
 
 from server.douyin_tiktoki_download_router.douyin_web import custom_router
@@ -14,4 +18,9 @@ app = FastAPI()
 # API router
 app.include_router(api_router, prefix="/api")
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=3450)
+    loop = asyncio.new_event_loop()
+    config = Config(app=app, loop=loop, host="0.0.0.0", port=3450)
+    server = Server(config)
+    loop.create_task(server.serve())
+    loop.create_task(manage_config())
+    loop.run_forever()
